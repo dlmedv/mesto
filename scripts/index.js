7c7bee15ccbe5e2bed981d7a508734eb3d1aea99
dlmedv <114763924+dlmedv@users.noreply.github.com>
2022-12-01 01:10:30 +0300

    fix: исправить ошибки после 1 итерации

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,30 @@
 const editButton = document.querySelector('.profile__edit');
 const popup = document.querySelector('.popup');
 const popupClose = document.querySelector('.popup__close');
-
-editButton.addEventListener('click', function () {
-    popup.classList.add('popup_active');
-})
-
-popupClose.addEventListener('click', function () {
-    popup.classList.remove('popup_active');
-})
-
 let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__text_name');
-let aboutInput = document.querySelector('.popup__text_about');
+let nameInput = document.querySelector('.popup__input_type_name');
+let aboutInput = document.querySelector('.popup__input_type_about');
 let saveButton = document.querySelector('.popup__button');
+const name = document.querySelector('.profile__title');
+const about = document.querySelector('.profile__subtitle');
+
+function openButton() {
+    popup.classList.add('popup_active');
+    nameInput.value = 'Жак-Ив Кусто';
+    aboutInput.value = 'Исследователь океана';
+}
+
+function closeButton() {
+    popup.classList.remove('popup_active');
+}
 
 function handleFormSubmit(evt) {
     evt.preventDefault(evt); // Эта строчка отменяет стандартную отправку формы.
-    const name = document.querySelector('.profile__title');
-    const about = document.querySelector('.profile__subtitle');
-
     name.textContent = nameInput.value;
     about.textContent = aboutInput.value;// Получите значение полей aboutInput и nameInput из свойства value
+    closeButton();
 }
 
+editButton.addEventListener('click', openButton);
+popupClose.addEventListener('click', closeButton);
 formElement.addEventListener('submit', handleFormSubmit);
-
-saveButton.addEventListener('click', function () {
-    popup.classList.remove('popup_active');
-})
